386a57f100596d706600ee5cba68f1ca87f43209
Soumyanil Das <dassic@outlook.com>
2023-02-16 02:15:49 +0530

    added db integration and better error handling

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
@@ -18,25 +19,40 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            message: "Creation of post failed due to wrongly entered data",
-            errors: errors
-        });
+        const error = new Error('Creation of post failed due to wrongly entered data');
+        error.statusCode = 422;
+        throw error;
+        // return res.status(422).json({
+        //     message: "Creation of post failed due to wrongly entered data",
+        //     errors: errors
+        // });
 
     }
     const title = req.body.title;
     const content = req.body.content;
 
-    res.status(201).json({
-        message: "Post created successfully!",
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: {
-                name: 'Soumyanil'
-            },
-            createdAt: new Date()
+    const post = new Post({
+        title: title,
+        content: content,
+        creator: {
+            name: 'Soumyanil'
         },
+        imageUrl: 'images/jets.JPEG'
     });
+
+    post.save()
+        .then(result => {
+            // console.log(result);
+            res.status(201).json({
+                message: "Post created successfully!",
+                post: result,
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+
 }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const Path = require('path');
 const express = require('express');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
@@ -7,6 +8,7 @@ const app = express();
 const feedRouter = require('./routes/feed');
 
 app.use(bodyparser.json());     // We wouldn't use urlencoded here because we are only gonna deal with json data both with req and res
+app.use('/images', express.static(Path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,6 +18,15 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed', feedRouter);
+
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+
+    res.status(status).json({ message: message });
+})
 
 mongoose.connect('mongodb+srv://dassic:Dassic007@cluster0.ad9yl.mongodb.net/test')
     .then(res => {
